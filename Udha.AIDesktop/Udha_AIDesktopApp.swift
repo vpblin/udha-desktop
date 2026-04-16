756858c3d140fd96b0a9c06c0100aa7f20974153
Blin Kazazi <blin@amk.solutions>
2026-04-16 14:04:58 -0400

    Initial Commit

---
+++ Udha.AIDesktop/Udha_AIDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udha_AIDesktopApp.swift
+//  Udha.AIDesktop
+//
+//  Created by Blin Kazazi on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Udha_AIDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Udha.AIDesktopTests/Udha_AIDesktopTests.swift
@@ -0,0 +1,17 @@
+//
+//  Udha_AIDesktopTests.swift
+//  Udha.AIDesktopTests
+//
+//  Created by Blin Kazazi on 4/16/26.
+//
+
+import Testing
+@testable import Udha_AIDesktop
+
+struct Udha_AIDesktopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
